d7a42afc50ee9e1d83318960ac6ae35d97eb4caf
RohitKumarMahadev <rohit.gmrit@gmail.com>
2021-04-27 16:54:18 +0530

    Updated OGImage

--- components/layout.js
+++ components/layout.js
@@ -20,7 +20,7 @@ export default function Layout({ children, home }) {
         />
         <meta
           property="og:image"
-          content={`https://rohitnotes.com/images/OGImage.jpeg`}
+          content={`https://rohitnotes.com/images/OGHeader.jpeg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
